b6f5ed58c321411ad3e51025af4d28a88686ffaa
Edgar <edgar@anotherfoxguy.com>
2019-07-20 11:38:10 +0200

    :sparkles: Released 1.4.3

--- latest-info.cmake
+++ latest-info.cmake
@@ -7,7 +7,7 @@ function(_pmm_changes version)
     endif()
 endfunction()
 
-set(PMM_LATEST_VERSION 1.4.1)
+set(PMM_LATEST_VERSION 1.4.3)
 
 if(PMM_VERSION VERSION_LESS PMM_LATEST_VERSION AND NOT PMM_IGNORE_NEW_VERSION)
     message(STATUS "[pmm] You are using PMM version ${PMM_VERSION}. The latest is ${PMM_LATEST_VERSION}.")
@@ -76,6 +76,10 @@ if(PMM_VERSION VERSION_LESS PMM_LATEST_VERSION AND NOT PMM_IGNORE_NEW_VERSION)
         )
     _pmm_changes(1.4.2
         "Upgrade to Conan 1.16"
+        )
+    _pmm_changes(1.4.3
+        "Upgrade to Conan 1.17"
+        "Fixed latest version"
         )
     message(STATUS "[pmm] To update, simply change the value of PMM_VERSION_INIT in pmm.cmake")
     message(STATUS "[pmm] You can disable these messages by setting PMM_IGNORE_NEW_VERSION to TRUE before including pmm.cmake")
