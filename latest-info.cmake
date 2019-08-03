750d2277190c527054cec6b3272f228351562380
Edgar <Edgar@AnotherFoxGuy.com>
2019-08-03 16:06:46 +0200

    :tada: Prepare for 1.5 release

--- latest-info.cmake
+++ latest-info.cmake
@@ -1,90 +1,94 @@
 function(_pmm_changes version)
-    if(PMM_VERSION VERSION_LESS version)
+    if (PMM_VERSION VERSION_LESS version)
         message(STATUS "[pmm]  - Changes in ${version}:")
-        foreach(change IN LISTS ARGN)
+        foreach (change IN LISTS ARGN)
             message(STATUS "[pmm]     - ${change}")
-        endforeach()
-    endif()
+        endforeach ()
+    endif ()
 endfunction()
 
-set(PMM_LATEST_VERSION 1.4.3)
+set(PMM_LATEST_VERSION 1.5.0)
 
-if(PMM_VERSION VERSION_LESS PMM_LATEST_VERSION AND NOT PMM_IGNORE_NEW_VERSION)
+if (PMM_VERSION VERSION_LESS PMM_LATEST_VERSION AND NOT PMM_IGNORE_NEW_VERSION)
     message(STATUS "[pmm] You are using PMM version ${PMM_VERSION}. The latest is ${PMM_LATEST_VERSION}.")
     message(STATUS "[pmm] Changes since ${PMM_VERSION} include the following:")
     _pmm_changes(0.2.0
-        "Automatic update checks"
-        "CONAN uses the `cmake` generator and automatically defines imported targets"
-        "SETTINGS for Conan"
-        "OPTIONS for Conan"
-        "BUILD for setting the Conan install '--build' option"
-        )
+            "Automatic update checks"
+            "CONAN uses the `cmake` generator and automatically defines imported targets"
+            "SETTINGS for Conan"
+            "OPTIONS for Conan"
+            "BUILD for setting the Conan install '--build' option"
+            )
     _pmm_changes(0.3.0
-        "MSVC Support and Fixes"
-        )
+            "MSVC Support and Fixes"
+            )
     _pmm_changes(0.3.1
-        "Install virtualenv in a user-local path"
-        )
+            "Install virtualenv in a user-local path"
+            )
     _pmm_changes(0.4.0
-        "Some utilities when running as `cmake -P pmm.cmake`"
-        "Pass `build_type` setting to Conan"
-        "[Windows] Install to Local AppData instead of Roaming"
-        )
+            "Some utilities when running as `cmake -P pmm.cmake`"
+            "Pass `build_type` setting to Conan"
+            "[Windows] Install to Local AppData instead of Roaming"
+            )
     _pmm_changes(1.0.0
-        "Support for vcpkg"
-        )
+            "Support for vcpkg"
+            )
     _pmm_changes(1.0.1
-        "Fixes for building a CMake project inside of the Conan local cache"
-        )
+            "Fixes for building a CMake project inside of the Conan local cache"
+            )
     _pmm_changes(1.0.2
-        "DEBUG and VERBOSE logging options."
-        )
+            "DEBUG and VERBOSE logging options."
+            )
     _pmm_changes(1.0.3
-        "Fix using Conan with a too-new GCC version: Only use the major version on GCC 5 and later"
-        )
+            "Fix using Conan with a too-new GCC version: Only use the major version on GCC 5 and later"
+            )
     _pmm_changes(1.1.0
-        "Support a REMOTES argument in Conan mode to add remotes before performing installation"
-        )
+            "Support a REMOTES argument in Conan mode to add remotes before performing installation"
+            )
     _pmm_changes(1.1.1
-        "[experimental] Preliminary support for libman"
-        "Now installs latest supported version of Conan rather than a specific version"
-        )
+            "[experimental] Preliminary support for libman"
+            "Now installs latest supported version of Conan rather than a specific version"
+            )
     _pmm_changes(1.2.0
-        "Fix issues with finding Python in some Windows setups"
-        "CMakeCM support"
-        )
+            "Fix issues with finding Python in some Windows setups"
+            "CMakeCM support"
+            )
     _pmm_changes(1.3.0
-        "/Install, /Upgrade, and /Uninstall arguments to the script to support controlling PMM's Conan installation"
-        "pmm(CONAN) args: BINCRAFTERS and COMMUNITY to enable the Bincrafters and conan-community repositories, respectively"
-        "Generate a Conan profile file in the build directory for use by the user, instead of passing all command line args"
-        "Don't try to re-download already obtained files"
-        "Respect PYENV_ROOT when looking for Python"
-        "PMM DEBUG mode now prints all external commands that it executes"
-        "PMM DEBUG prints debugging information from vcpkg bootstrap"
-        )
+            "/Install, /Upgrade, and /Uninstall arguments to the script to support controlling PMM's Conan installation"
+            "pmm(CONAN) args: BINCRAFTERS and COMMUNITY to enable the Bincrafters and conan-community repositories, respectively"
+            "Generate a Conan profile file in the build directory for use by the user, instead of passing all command line args"
+            "Don't try to re-download already obtained files"
+            "Respect PYENV_ROOT when looking for Python"
+            "PMM DEBUG mode now prints all external commands that it executes"
+            "PMM DEBUG prints debugging information from vcpkg bootstrap"
+            )
     _pmm_changes(1.3.1
-        "Fix changes to build settings and options not triggering a Conan installation"
-        )
+            "Fix changes to build settings and options not triggering a Conan installation"
+            )
     _pmm_changes(1.4.0
-        "Fixed missing os_build && arch_build from conan profile"
-        "Added support for VS 2019"
-        "Fixed finding python on AppVeyor"
-        "Added VCPKG PORTS for copying custom ports"
-        )
+            "Fixed missing os_build && arch_build from conan profile"
+            "Added support for VS 2019"
+            "Fixed finding python on AppVeyor"
+            "Added VCPKG PORTS for copying custom ports"
+            )
     _pmm_changes(1.4.1
-        "Added Clean helper command"
-        )
+            "Added Clean helper command"
+            )
     _pmm_changes(1.4.2
-        "Upgrade to Conan 1.16"
-        )
+            "Upgrade to Conan 1.16"
+            )
     _pmm_changes(1.4.3
-        "Upgrade to Conan 1.17"
-        "Fixed latest version"
-        )
+            "Upgrade to Conan 1.17"
+            "Fixed latest version"
+            )
+    _pmm_changes(1.5.0
+            "Upgrade to Conan 1.18"
+            "Added support for Conan cmake_multi generator"
+            )
     message(STATUS "[pmm] To update, simply change the value of PMM_VERSION_INIT in pmm.cmake")
     message(STATUS "[pmm] You can disable these messages by setting PMM_IGNORE_NEW_VERSION to TRUE before including pmm.cmake")
-endif()
+endif ()
 
-if(NOT DEFINED _PMM_BOOTSTRAP_VERSION OR _PMM_BOOTSTRAP_VERSION LESS 1)
+if (NOT DEFINED _PMM_BOOTSTRAP_VERSION OR _PMM_BOOTSTRAP_VERSION LESS 1)
     message(STATUS "[pmm] NOTE: pmm.cmake has changed! Please download a new pmm.cmake from the PMM repository.")
-endif()
+endif ()
